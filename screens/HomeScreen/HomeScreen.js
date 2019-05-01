ba904057db0f741824d63799c3958941b8dbb95f
Christian Morales <cmorale1@trinity.edu>
2019-05-01 00:05:37 -0500

    Implemented Hard-coded Google Map

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import {View, Text, Dimensions, StyleSheet, ScrollView} from 'react-native';
+import { MapView } from 'expo';
 import { Container, Content, Button } from 'native-base';
+
+let width = Dimensions.get('window').width;
+let height = Dimensions.get('window').height;
 
 export default class HomeScreen extends React.Component {
     render() {
         return (
             <Container>
-                <Content style={{flex: 1, backgroundColor: 'powderblue'}}>
-
+                <Content style={{flex: 1}}>
+                    <MapView
+                        style={{width: width, height: height}}
+                        initialRegion={{
+                            latitude: 37.78825,
+                            longitude: -122.4324,
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421,
+                        }}
+                    />
                     <View style={[styles.mapButtons]}>
                         <Button style={[styles.mapLeftButton]}>
                             <Text style={{fontWeight: 'bold'}}>ESCORT</Text>
@@ -24,10 +36,11 @@ export default class HomeScreen extends React.Component {
 
 const styles = StyleSheet.create({
     mapButtons: {
+        position: 'absolute',
         flexDirection: 'row',
-        alignItems: 'center',
+        top: '70%',
+        left: 21,
         justifyContent: 'center',
-        flex: 1,
     },
     mapLeftButton: {
         width: 150,
